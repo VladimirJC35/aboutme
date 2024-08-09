c1db6ba6313a7ec5146622e97ef18045f71699a3
VladimirJC35 <vladimirjc25@gmail.com>
2024-08-09 00:09:19 -0500

    creando pagina web about me

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # aboutme
-Perfil Profesional
+Proyecto: Web sobre mi Perfil Profesional
+### Autor: Vladimir Jáuregui Canchari
+
+### Enlaces y Recursos
+
+* []()
+* [enlace de github](https://github.com/entertechschool/lima-code-201n3/blob/main/configs/.gitignore)
+
+## Pagina web sobre mi perfil
+
+### Estructura del Proyecto
+* Identificación del usuario
+* Juego de adivinanzas
+* Resultado de preguntas correctas
+* Presentación inicial de  página web personal
+* Evaluación final de accesibilidad es 91%
+![Accesibilidad Web](./img/AccesibleWebLab02.jpg)
+
+### Reflexiones y Comentarios
---
+++ js/app.js
@@ -0,0 +1,50 @@
+//'use strict';
+const respuesta1 = "No";
+const respuesta2 = "Si";
+const respuesta3 = "Si";
+const respuesta4 = "No";
+const respuesta5 = "No";
+//const numeroMaxIntentos = 4;
+//const numeroAAdivinar = 43;
+//let contadorDeIntentos = 1;
+//const colorfavorito = ["Azul Marino", "Verde", "Turquesa","Morado"];
+//const numMaxIntento = 6;
+//let contarIntento= 1;
+
+let contadorbien = 0;
+let contadormal = 0;
+puntos = 0;
+//let mensaje = usuario + " Tiene " + puntos + " respuestas correctas ";
+let usuario = prompt("Hola; ¿ Cual es tu nombre/apodo ?");
+alert('Bienvenido, mucho gusto ' + usuario)
+const pregunta1 = prompt("¿Tengo un segundo nombre?, Si / No");
+if (respuesta1.toLowerCase() === pregunta1.toLowerCase()) {
+    alert("Respuesta Correcta, no pense que adivinarias ")
+    puntos += 1;
+} else { alert("Respuesta Incorrecta") }
+
+const pregunta2 = prompt("¿Trabajo actualmente?, Si / No?");
+if (respuesta2.toLowerCase() === pregunta2.toLowerCase()) {
+    alert("Respuesta Correcta")
+    puntos += 1;
+} else { alert("Respuesta Incorrecta") }
+
+const pregunta3 = prompt("¿Nací en Ayacuho?, Si / No");
+if (respuesta3.toLowerCase() === pregunta3.toLowerCase()) {
+    alert("Respuesta Correcta")
+    puntos += 1;
+} else { alert("Respuesta Incorrecta") }
+
+const pregunta4 = prompt("¿Inicié a estudiar Ingeniería, en Lima?, Si / No");
+if (respuesta4.toLowerCase() === pregunta4.toLowerCase()) {
+    alert("Respuesta correcta")
+    puntos += 1;
+} else { alert("Respuesta Incorrecta") }
+
+const pregunta5 = prompt("¿Tengo el titulo de la Maestría?, Si / No");
+if (respuesta5.toLowerCase() === pregunta5.toLowerCase()) {
+    alert("Respuesta correcta")
+    puntos += 1;
+} else { alert("Respuesta Incorrecta") }
+console.log(usuario+"tiene"+puntos);
+alert ("muchas gracias " + usuario + "  En esta primera parte, Ud. tiene:  "+ puntos+"  Repuestas correctas");
