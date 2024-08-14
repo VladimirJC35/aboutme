db2ad18cf7ed324c7f0499636b2f53efe574b32d
VladimirJC35 <vladimirjc25@gmail.com>
2024-08-14 07:05:39 -0500

    Incremento preguntas

--- js/app.js
+++ js/app.js
@@ -4,17 +4,18 @@ const respuesta2 = "Si";
 const respuesta3 = "Si";
 const respuesta4 = "No";
 const respuesta5 = "No";
-//const numeroMaxIntentos = 4;
-//const numeroAAdivinar = 43;
-//let contadorDeIntentos = 1;
-//const colorfavorito = ["Azul Marino", "Verde", "Turquesa","Morado"];
-//const numMaxIntento = 6;
+
+const numeroMaxIntentos = 4;
+const numeroAAdivinar = 15;
+let contadorDeIntentos = 1;
+const colorfavorito = ["Azul Marino", "Verde", "Turquesa","Morado"];
+const numMaxIntento = 6;
 //let contarIntento= 1;
 
 let contadorbien = 0;
 let contadormal = 0;
 puntos = 0;
-//let mensaje = usuario + " Tiene " + puntos + " respuestas correctas ";
+
 let usuario = prompt("Hola; ¿ Cual es tu nombre/apodo ?");
 alert('Bienvenido, mucho gusto ' + usuario)
 const pregunta1 = prompt("¿Tengo un segundo nombre?, Si / No");
@@ -48,3 +49,45 @@ if (respuesta5.toLowerCase() === pregunta5.toLowerCase()) {
 } else { alert("Respuesta Incorrecta") }
 console.log(usuario+"tiene"+puntos);
 alert ("muchas gracias " + usuario + "  En esta primera parte, Ud. tiene:  "+ puntos+"  Repuestas correctas");
+
+while (contadorDeIntentos <= numeroMaxIntentos) {
+    const numeroPorUsuario = Number(prompt("¿Cuantos años de experiencia tengo como Analista de S.I ?, ingresa Números: "+numeroMaxIntentos+" intentos"));
+
+    if (numeroPorUsuario === numeroAAdivinar) {
+        alert("Felicidades!!! adivinaste amigo(a) tengo " + numeroAAdivinar+"años de experiencia")
+        alert("Lo hiciste en " + contadorDeIntentos + " intentos");
+        puntos += 1;
+        break;
+    } if (numeroPorUsuario > numeroAAdivinar) {
+        alert(numeroPorUsuario + "  Estimación demasiado alta ");
+    } else {
+        alert(numeroPorUsuario + "  Estimación demasiado baja ")
+
+    }
+    contadorDeIntentos++;
+    if (contadorDeIntentos === numeroMaxIntentos + 1) {
+        alert("Llegaste al número max de intentos :(, Mi experiencia es de: "+numeroAAdivinar);
+    }
+}
+
+let coloringresado = prompt ("Adivina mi color favorito...!,Tienes " +numMaxIntento + " intentos..")
+
+for(let i=1; i<=numMaxIntento;i++) {
+
+    for (let j=0; j< colorfavorito.length; j++) {
+        if(coloringresado.toUpperCase() == colorfavorito[j].toUpperCase() ) {
+         alert ("Acertaste !!! Felicitaciones!!");
+         contadorDeIntentos++;
+         puntos += 1;
+         break;
+        }
+        else{
+            coloringresado = prompt("No acertaste..! Sigue intentando te quedan " + (numMaxIntento-i) + " intentos. El color es:");
+        }
+    } if (i==numMaxIntento) {
+        alert ("Mis colores Favoritos son: "+ colorfavorito +" Éxitos en tu opróximo intento ...!");
+    }
+       
+ 
+}
+alert (usuario+" Gracias por participar. Ud. tiene un Total de :  "+ puntos+"  Repuestas correctas");
